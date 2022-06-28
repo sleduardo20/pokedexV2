803bcf451cb9e868b3cbd80838f640dfba4b0f84
sleduardo20 <sl.eduardo20@gmail.com>
2022-06-28 16:36:36 -0300

    🎨 feat - create theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export const theme = {
+  colors: {
+    type: {
+      rock: "#b69e31",
+      ghost: "#70559b",
+      steel: "#b7b9d0",
+      water: "#6493eb",
+      grass: "#74cb48",
+      psychic: "#fb5584",
+      ice: "#9ad6df",
+      dark: "#75574c",
+      fairy: "#e69eac",
+      normal: "#aaa67f",
+      fighting: "#c12239",
+      flying: "#a891ec",
+      poison: "#a43e9e",
+      ground: "#dec16b",
+      bug: "#a7b723",
+      fire: "#f57d31",
+      electric: "#f9cf30",
+      dragon: "#7037ff",
+    },
+    black: {
+      darkGray: "#212121",
+      mediumGray: "#666666",
+      lightGray: "#e0e0e0",
+      white: "#ffffff",
+      background: "#f7f7f7",
+    },
+  },
+} as const;
