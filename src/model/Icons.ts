529adc9735fc919f8dd76356950e6d3b01de894e
sleduardo20 <sl.eduardo20@gmail.com>
2022-06-28 17:26:31 -0300

    ✨ feat - create model Icons

---
+++ src/model/Icons.ts
@@ -0,0 +1,23 @@
+import {
+  AiOutlineSortAscending as IconSortAscending,
+  AiOutlineSortDescending as IconSortDescending,
+  AiOutlineSearch as IconSearch,
+  AiOutlineHourglass as IconWeight,
+} from "react-icons/ai";
+
+import {
+  FiChevronRight as IconArrowRight,
+  FiArrowLeft as IconArrowLeft,
+} from "react-icons/fi";
+
+import { RiLineHeight as IconHeight } from "react-icons/ri";
+
+export {
+  IconSortAscending,
+  IconSortDescending,
+  IconArrowRight,
+  IconArrowLeft,
+  IconSearch,
+  IconHeight,
+  IconWeight,
+};
