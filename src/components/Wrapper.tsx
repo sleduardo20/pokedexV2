61a6b879bd22fae1aa80499eb438068c69124ce5
sleduardo20 <sl.eduardo20@gmail.com>
2022-06-29 16:50:34 -0300

    ✨ feat - create Wrapper component

---
+++ src/components/Wrapper.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+  height: 100%;
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
