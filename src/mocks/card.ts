a43a61ae42538bc55d4a984f340ab95a890c9988
sleduardo20 <sl.eduardo20@gmail.com>
2022-07-24 21:30:03 -0300

    ✨ feat - create mock card

---
+++ src/mocks/card.ts
@@ -0,0 +1,9 @@
+import { PokemonType } from "../models/PokemonType";
+
+export default {
+  id: 121,
+  name: "bulbasaur",
+  urlImage:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+  type: PokemonType.Grass,
+};
