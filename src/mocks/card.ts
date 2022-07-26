b3610e5cf4811484f3aca6bd6a1afd988b5ca975
sleduardo20 <sl.eduardo20@gmail.com>
2022-07-25 23:21:01 -0300

    ♻ chore - create mock cards

--- src/mocks/card.ts
+++ src/mocks/card.ts
@@ -1,9 +1,32 @@
 import { PokemonType } from "../models/PokemonType";
 
-export default {
-  id: 121,
-  name: "bulbasaur",
-  urlImage:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
-  type: PokemonType.Grass,
-};
+export default [
+  {
+    id: 121,
+    name: "bulbasaur",
+    urlImage:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
+    type: PokemonType.Grass,
+  },
+  {
+    id: 122,
+    name: "ivysaur",
+    urlImage:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
+    type: PokemonType.Grass,
+  },
+  {
+    id: 899,
+    name: "scyther",
+    urlImage:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/123.svg",
+    type: PokemonType.Bug,
+  },
+  {
+    id: 144,
+    name: "articuno",
+    urlImage:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/144.svg",
+    type: PokemonType.Ice,
+  },
+];
