c6728dea94cee3768288e0088b136b65eb4bfca6
sleduardo20 <sl.eduardo20@gmail.com>
2022-07-31 16:31:01 -0300

    ✨ feat - add hook usePokemons

--- src/presentation/pages/search/Search.tsx
+++ src/presentation/pages/search/Search.tsx
@@ -1,16 +1,33 @@
-/* eslint-disable no-unused-vars */
-import { FormEvent, useState } from "react";
+import { FormEvent, useMemo, useState } from "react";
+
 import { Wrapper } from "../../components/Wrapper";
-import { Container, Header, Logo } from "./styles";
-import logoPokedex from "../../assets/logoPokedex.svg";
 import { Input } from "../../components/Input";
 import { IconSortAscending } from "../../components/Icons";
 import { ListCards } from "../../components/ListCards";
 
-import cardMock from "../../../mocks/card";
+import logoPokedex from "../../assets/logoPokedex.svg";
+import { usePokemons } from "../../hooks/usePokemons";
+import { PokemonType } from "../../../models/PokemonType";
+import { Container, Header, Logo } from "./styles";
 
 export function Search() {
   const [search, setSearch] = useState("");
+  const [loadingMore, setLoadingMore] = useState(0);
+
+  const { pokemons, loading } = usePokemons(loadingMore);
+
+  console.log(loading, pokemons);
+
+  const cards = useMemo(
+    () =>
+      pokemons.map(({ id, name, types, sprites }) => ({
+        id,
+        name,
+        urlImage: sprites.other.dream_world.front_default,
+        type: types[0].type.name as PokemonType,
+      })),
+    [pokemons]
+  );
 
   const handleClearSearch = () => {
     console.log("clear value");
@@ -37,7 +54,10 @@ export function Search() {
           />
         </form>
 
-        <ListCards cards={cardMock} />
+        <ListCards cards={cards} />
+        <button type="button" onClick={() => setLoadingMore(loadingMore + 1)}>
+          loading more
+        </button>
       </Container>
     </Wrapper>
   );
