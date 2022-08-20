f05e7214d471477956ee972830e292321debb767
sleduardo20 <sl.eduardo20@gmail.com>
2022-08-19 21:12:55 -0300

    ✔ create test in PokemonService

---
+++ src/application/tests/PokedexGateway.test.ts
@@ -0,0 +1,36 @@
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import { listPokeomonMock, pokemonMock } from "../../mocks/pokemon";
+import { ListPokemons, Pokemon } from "../../models/PokemonType";
+
+import { pokedexService } from "../UseServices";
+
+const handles = [
+  rest.get("*/pokemon/pikachu", (_, res, ctx) => {
+    return res(ctx.json(pokemonMock as Pokemon));
+  }),
+  rest.get("*/pokemon/?offset=25&limit=25", (_, res, ctx) => {
+    return res(ctx.json(listPokeomonMock as ListPokemons));
+  }),
+];
+
+const server = setupServer(...handles);
+
+describe("PokedexGateway", () => {
+  beforeEach(() => {
+    server.listen();
+  });
+
+  afterAll(() => {
+    server.close();
+  });
+
+  it("should be able get pokemons list and pokemons", async () => {
+    const resultPokemon = await pokedexService.getPokemon("pikachu");
+
+    const resultListPokemon = await pokedexService.getListPokemon("?limit=25");
+
+    expect(resultPokemon).toEqual(pokemonMock);
+    expect(resultListPokemon).toEqual(listPokeomonMock);
+  });
+});
