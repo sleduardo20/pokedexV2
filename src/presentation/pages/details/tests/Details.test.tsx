479681fe9de355ffc37501101a7c29131ee89145
sleduardo20 <sl.eduardo20@gmail.com>
2022-08-23 18:09:24 -0300

    ✔ adjusts test

--- src/presentation/pages/details/tests/Details.test.tsx
+++ src/presentation/pages/details/tests/Details.test.tsx
@@ -3,8 +3,7 @@ import { renderWithTheme } from "../../../utils/tests";
 import { Details } from "../Details";
 
 describe("Details", () => {
-  const props = {} as Pokemon;
   it("should be able render Details page correctly", () => {
-    expect(renderWithTheme(<Details {...props} />)).toBeTruthy();
+    expect(renderWithTheme(<Details />)).toBeTruthy();
   });
 });
