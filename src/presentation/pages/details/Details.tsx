241934c069b8540ce3aadbfa7a6a386926fe37eb
sleduardo20 <sl.eduardo20@gmail.com>
2022-08-24 17:12:00 -0300

    add context usePokedex

--- src/presentation/pages/details/Details.tsx
+++ src/presentation/pages/details/Details.tsx
@@ -1,30 +1,41 @@
-import { Pokemon, PokemonType } from "../../../models/PokemonType";
+import { useParams } from "react-router-dom";
+import { PokemonType } from "../../../models/PokemonType";
 import { IconArrowLeft } from "../../components/Icons";
 import { Container } from "./styles";
 import { pokemonMock } from "../../../mocks/pokemon";
+import { usePokedex } from "../../contexts/usePokedex";
 
-export function Details({
-  id,
-  height,
-  name,
-  sprites,
-  moves,
-  stats,
-  types,
-  weight,
-}: Pokemon) {
+export function Details() {
+  const { name } = useParams();
+  const { pokemons } = usePokedex();
+
+  const pokemonDetails = pokemons.filter((p) => p.name === name)[0];
+
+  const backgrondTypeColor = pokemonDetails.types[0].type.name as PokemonType;
+
+  const currentPokemon = pokemons.indexOf(pokemonDetails);
+
+  const nextPokemon = pokemons.filter(
+    (_, index) => index === currentPokemon + 1
+  )[0];
+
+  const previousPokemon = pokemons.filter(
+    (_, index) => index === currentPokemon - 1
+  )[0];
+
+  console.log({ pokemons, pokemonDetails, nextPokemon, previousPokemon });
   return (
-    <Container background={PokemonType.Bug}>
+    <Container background={backgrondTypeColor}>
       <header>
         <IconArrowLeft />
-        <strong>{pokemonMock.name}</strong>
-        <span>#{pokemonMock.id}</span>
+        <strong>{pokemonDetails.name}</strong>
+        <span>#{pokemonDetails.id}</span>
       </header>
       <main>
         <picture>
           <img
-            src={`${pokemonMock.sprites.other.dream_world}`}
-            alt={pokemonMock.name}
+            src={`${pokemonDetails.sprites.other.dream_world.front_default}`}
+            alt={pokemonDetails.name}
           />
         </picture>
       </main>
