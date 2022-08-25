11ae9d8d9c0a1a13413bd0ced1a4ef454d4f0d98
sleduardo20 <sl.eduardo20@gmail.com>
2022-08-25 19:16:57 -0300

    🎨 add styles in Details page

--- src/presentation/pages/details/styles.ts
+++ src/presentation/pages/details/styles.ts
@@ -2,5 +2,45 @@ import styled from "styled-components";
 import { PokemonType } from "../../../models/PokemonType";
 
 export const Container = styled.div<{ background: PokemonType }>`
+  margin: 4rem 1.6rem;
+  padding: 0.8rem;
   background-color: ${({ theme, background }) => theme.colors.type[background]};
+  height: 84rem;
+  border-radius: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  header {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: ${({ theme }) => theme.colors.black.white};
+
+    svg {
+      cursor: pointer;
+    }
+
+    strong {
+      text-transform: capitalize;
+      font-size: 2.8rem;
+      font-weight: 500;
+    }
+  }
+
+  main {
+    margin-top: 8rem;
+    z-index: 10;
+  }
+
+  article {
+    margin-top: -4rem;
+    z-index: 1;
+    width: 100%;
+    height: 100%;
+    border-radius: 1rem;
+
+    background-color: ${({ theme }) => theme.colors.black.white};
+  }
 `;
